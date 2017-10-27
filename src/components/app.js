973b9a9910be754d0fe230cb63f41b4376ea24d9
j-christoffersen <jacksontchristo@gmail.com>
2017-10-27 16:04:34 -0700

    implement videoList and videoPlayer

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,17 @@
 angular.module('video-player')
 
 .component('app', {
-  // TODO
+  controller: function() {
+    this.videos = window.exampleVideoData;
+    this.currentVideo = this.videos[0];
+    
+    this.selectVideo = function (index) {
+      this.currentVideo = this.videos[index];
+    }.bind(this);
+    
+    this.searchResults = function() {
+      // TODO
+    };
+  },
+  templateUrl: 'src/templates/app.html'
 });
